f05523380a355180fa0f798107236d38f617677f
gi3vnny <giovannylindor56@gmail.com>
2025-10-06 17:35:52 -0400

    Started JavaScript Button Logic, Tweaked HTML & CSS.

--- src/script.js
+++ src/script.js
@@ -1,2 +1,20 @@
+//grabs all btns
+const allBtns = document.querySelectorAll('input[type="button"]');
+//grabs display
+const display = document.getElementById('display');
 
+//click event for each btn
+allBtns.forEach(displayBtn);
+//btn logic for each click event
+function displayBtn (item, index, arr) {
 
+    item.addEventListener('click', () => {
+        if (item.value.toString() == 'AC') {
+            display.value = '';
+        } else if (item.value.toString() == '=') {
+            display.value = eval(display.value);
+        }else {
+             display.value += item.value;
+        }
+    })
+}
