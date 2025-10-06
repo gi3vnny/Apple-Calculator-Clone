89e530472a3c3a4c18e9646616dd3da69c3e07a9
gi3vnny <giovannylindor56@gmail.com>
2025-10-06 19:42:54 -0400

    Included pseudocode for calculator logic in comments of JS File.

--- src/script.js
+++ src/script.js
@@ -18,3 +18,41 @@ function displayBtn (item, index, arr) {
         }
     })
 }
+
+/* 
+                                MAIN CALC LOGIC
+    'input[type="button"]'; == calls an index of buttons, which is > 1
+    forEach button, we display the value in the display area. 
+    
+    Main Logic:
+    
+    if the user hits the '=' btn, program will perform operation thats on the screen
+
+    if the user hits 'AC' btn, program will clear the entire display. 
+
+    if the user hits the multiply SYMBOL or the divide SYMBOL, program will convert the X symbol => '*' OR program will convert divide symbol => /
+
+    if the user hits a number && hits the '+/-' sign, it will toggle between a signed and a whole number. 
+
+    if the user hits the '%' btn, the program will interpret the btn as a percentage & when it evaulates, it comes down to num / 100
+*/
+
+
+/* 
+                            ADDITIONAL FEATURES 
+    - the display, will be automatically set to 0.
+    ** If the user doesn't type a number, and hits the operators [/, *, -, +], the display will show 0, and then the operator. **
+
+    - while the user hits a btn != AC, AC btn will be converted to a backspace btn. backspace btn, removes one digit.
+
+    - An iPhone template screen with similar dimensions will be input behind the screen w/ Z-Index. 
+    - iPhone white bottom bar will be also implemented 
+*/
+
+
+
+/*                        FUTURE UPDATES
+    - Convert the bottom AC btn to the calculator symbol. 
+*/ 
+
+
